ec25d9ae6c783d7425c4ad61e59c099b9cfa0d5e
Matyáš Kumžák <kumzak.maty@gmail.com>
2025-08-12 18:55:20 +0200

    dasd

--- crawler.js
+++ crawler.js
@@ -1,10 +1,7 @@
 // crawler.js
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
-let PQueue;
-(async () => {
-  PQueue = (await import('p-queue')).default;
-})();
+const PQueue = require('p-queue').default;
 const { URL } = require('url');
 
 let playwright = null;
